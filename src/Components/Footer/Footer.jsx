b8a628d80c34253e8922888036b02c0fe9368918
jassirhashmi <jassirhashmi327@gmail.com>
2025-12-20 03:47:45 +0500

    update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,34 +4,38 @@ import footer_logo from '../../assets/footer_logo.png'
 import user_icon from '../../assets/user_icon.svg'
 
 const Footer = () => {
-  return (
-    <>
-    <div className="footer">
-        <div className="footer-top">
-            <div className="footer-top-left">
-                <img src={footer_logo} alt="" />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, minus! Eveniet unde voluptatibus temporibus quia laborum corrupti aliquam error in?</p>
-            </div>
-            <div className="footer-top-right">
-                <div className="footer-email-input">
-                    <img src={user_icon} alt="" />
-                    <input type="text" placeholder='Enter you email'/>
+    return (
+        <>
+            <div className="footer">
+                <div className="footer-top">
+                    <div className="footer-top-left">
+                        <img src={footer_logo} alt="" />
+                        <p>“Code. Design. Create.”
+
+                            “Making the web a better place, one site at a time.”
+
+                            “Crafting seamless experiences through clean code.”</p>
+                    </div>
+                    <div className="footer-top-right">
+                        <div className="footer-email-input">
+                            <img src={user_icon} alt="" />
+                            <input type="text" placeholder='Enter you email' />
+                        </div>
+                        <div className="footer-subscribe">Subscribe</div>
+                    </div>
                 </div>
-                <div className="footer-subscribe">Subscribe</div>
+                <hr />
+                <div className="footer-bottom">
+                    <p className="footer-bottom-left">© 2025 Jassir Hashmi. All rights reserved.</p>
+                    <div className="footer-bottom-right">
+                        <p>Term of Services</p>
+                        <p>Privacy Policy</p>
+                        <p>Connect with me</p>
+                    </div>
+                </div>
             </div>
-        </div>
-        <hr />
-        <div className="footer-bottom">
-            <p className="footer-bottom-left">© 2025 Jassir Hashmi. All rights reserved.</p>
-            <div className="footer-bottom-right">
-                <p>Term of Services</p>
-                <p>Privacy Policy</p>
-                <p>Connect with me</p>
-            </div>
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Footer
